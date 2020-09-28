33ecd7d38c0be60d252405e90f4e77cabd6babd7
karen <k.valeria.gonzalez@gmail.com>
2020-09-28 13:19:20 -0400

    Updates

---
+++ precache-manifest.48bfb73943747adadc84d55ac59b2135.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42b9964c124972c6eaa7b443e68301ef",
+    "url": "/project-five-karen-gonzalez/index.html"
+  },
+  {
+    "revision": "cbba9f1a908fb58e6a3e",
+    "url": "/project-five-karen-gonzalez/static/css/main.dd04b511.chunk.css"
+  },
+  {
+    "revision": "eb0c45fcdb3f13618cf0",
+    "url": "/project-five-karen-gonzalez/static/js/2.e62d4d33.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/project-five-karen-gonzalez/static/js/2.e62d4d33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbba9f1a908fb58e6a3e",
+    "url": "/project-five-karen-gonzalez/static/js/main.523a8206.chunk.js"
+  },
+  {
+    "revision": "6db81e18d379e5c2d75e",
+    "url": "/project-five-karen-gonzalez/static/js/runtime-main.18e50e16.js"
+  }
+]);
