5cdbdc939250666dae156ede0a686f679ffcd3bd
karen <k.valeria.gonzalez@gmail.com>
2020-09-28 15:25:26 -0400

    search changes

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -9,7 +9,7 @@ class SearchBar extends Component {
                 <h2>Contact list</h2>
                 <form action="" method="get">
                     <label forhtml="search"></label>
-                    <input onChange={this.props.handleChange} type="text" placeholder="search contact" name="search" required/>
+                    <input onChange={this.props.handleChange} value={this.props.contacts['']} type="text" placeholder="search contact" name="search" required/>
                     <label forhtml="Search"></label>
                     <input onClick={this.props.handleSearch} type="submit" value="search" />
                 </form>
