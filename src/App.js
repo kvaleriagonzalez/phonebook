a74593a4c0bae4f0ae351c4075262c072ce0ce62
karen <k.valeria.gonzalez@gmail.com>
2020-09-29 18:58:43 -0400

    adding api

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 
 import React, { Component } from 'react';
+import axios from "axios";
 import firebase from "./firebase";
 import SearchBar from './SearchBar';
 import FormSubmit from './FormSubmit';
 import './App.css';
 
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      contacts : [],
-      name : "",
-      number : "",
-      search : "",
+      contacts: [],
+      name: "",
+      number: "",
+      search: "",
       searchResults: [],
+      image: "https:cdn2.thecatapi.com/images/6ql.gif",
+      //Array("https://cdn2.thecatapi.com/images/6ql.gif",
+      //"https://cdn2.thecatapi.com/images/3ln.gif",
+      //"https://cdn2.thecatapi.com/images/2or.gif")
     };
   }
 
@@ -39,6 +45,8 @@ class App extends Component {
   });
   }
 
+
+
   // get user input and update the userInput state
   handleChange = (event) => {
     event.preventDefault();
@@ -48,27 +56,47 @@ class App extends Component {
     });
   };
 
-  // stop refreshing my page
-  // take user input and store in Firebase
-  // reset input field
-  handleClick = (event) => {
-    event.preventDefault();
-    // open portal to Firebase
-    const dbRef = firebase.database().ref();
-    const contacts = {
-      name : this.state.name,
-      number: this.state.number,
-    }
-    // add new record to Firebase
-    dbRef.push(contacts);
-    // reset input field
-    this.setState({
-      name: "",
-      number: "",
-    });
-  };
 
  
+      
+  handleClick = (event) => {
+     event.preventDefault();
+     axios({
+       url:
+         "https://api.thecatapi.com/v1/images/search/?api_key=85887931-8d04-4f23-a9fd-380b976e7e87&mime_types=gif",
+       method: "GET",
+       responseType: "JSON",
+       params: {
+         key: "85887931-8d04-4f23-a9fd-380b976e7e87",
+         mime_types: "gif",
+
+       },
+     }).then((response) => {
+       console.log(response.data);
+
+       // open portal to Firebase
+       const dbRef = firebase.database().ref();
+       const contacts = {
+         name: this.state.name,
+         number: this.state.number,
+         image: this.state.image,
+       };
+       // add new record to Firebase
+       dbRef.push(contacts);
+       // reset input field
+       this.setState({
+         name: "",
+         number: "",
+       });
+     });
+  };
+
+ //selectedRandomImage = () => {
+ //const randomImage = image[Math.floor(Math.random() * image.length)];
+//}
+
+
+
   // delete that specific contact from Firebase
   handleRemove = (contactDelete) => {
    // console.log(contactDelete);
@@ -99,7 +127,7 @@ class App extends Component {
       <header>
         <div className="wrapper">
             <h1>My PhoneBook</h1>
-              <div className="flex">
+              <div>
               <FormSubmit handleChange = {this.handleChange} contacts = {this.state.contacts} handleClick = {this.handleClick} />
               
               </div>  
@@ -109,7 +137,6 @@ class App extends Component {
                   this.state.contacts.map((contact,index) => { if (index > 2) {
                     return null
                   }
-
                     return (
                       <li key={contact.key}>
                         <p>
@@ -126,15 +153,19 @@ class App extends Component {
           <SearchBar handleChange={this.handleChange} contacts={this.state.contacts} handleSearch={this.handleSearch}/>
           <ul>
             {
-              // display  here
+              // display  here 
               this.state.searchResults.map((contact) => {
                 return (
                   <li key={contact.key}>
                     <p>
                       <span>Name:</span> {contact.contacts.name}<span> Number: </span>{contact.contacts.number}
-                      
                     </p>
-                    {/* button to delete contact */}
+                    <div>
+                      
+                          <img src={contact.contacts.image} alt="cat" />
+                      
+                        
+                    </div>
 
                   </li>
                 );
